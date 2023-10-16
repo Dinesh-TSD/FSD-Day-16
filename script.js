3a702758b6c029c11721dc25875c4c33589d4892
T.DHINESH <145428367+Dinesh-TSD@users.noreply.github.com>
2023-10-16 18:37:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+// let btn=document.getElementById("btn")
+
+// btn.addEventListener
+
+function counter() {
+
+   let display = document.getElementById("count")
+   let contain = document.getElementById("container")
+
+   setTimeout(function ten() {
+
+      display.innerHTML = "10"
+      let audi=document.createElement("audio")
+      audi.src="count.mp3"
+      audi.controls="true"
+      audi.autoplay="true"
+
+      display.append(audio)
+      
+   },)
+
+   setTimeout(function ten() {
+      display.innerHTML = "9"
+   }, 1000)
+
+   setTimeout(function ten() {
+      display.innerHTML = "8"
+   }, 2000)
+
+   setTimeout(function ten() {
+      display.innerHTML = "7"
+   }, 3000)
+
+   setTimeout(function ten() {
+      display.innerHTML = "6"
+   }, 4000)
+
+   setTimeout(function ten() {
+      display.innerHTML = "5"
+   }, 5000)
+
+   setTimeout(function ten() {
+      display.innerHTML = "4"
+   }, 6000)
+
+   setTimeout(function ten() {
+      display.innerHTML = "3"
+   }, 7000)
+
+   setTimeout(function ten() {
+      display.innerHTML = "2"
+   }, 8000)
+
+   setTimeout(function ten() {
+      display.innerHTML = "1"
+   }, 9000)
+
+   setTimeout(function ten() {
+      display.innerHTML = ""
+   }, 9500)
+
+   setTimeout(function ten() {
+      let video=document.createElement("video")
+      video.src="vid.mp4"
+      video.controls="true"
+      video.autoplay="true"
+
+      contain.append(video)
+   }, 10000)
+}
+
+
